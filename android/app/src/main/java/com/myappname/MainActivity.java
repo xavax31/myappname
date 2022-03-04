3c95d88b3ad93f5ca6793bcf54a53f7db1a1a3f1
xab <xavier.boisnon@gmail.com>
2022-03-04 11:15:41 +0100

    id process: reactnavigation

--- android/app/src/main/java/com/myappname/MainActivity.java
+++ android/app/src/main/java/com/myappname/MainActivity.java
@@ -1,6 +1,7 @@
 package com.myappname;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +13,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "myappname";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
 }
